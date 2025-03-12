a45050f712de1e07ab1ab543d9639c99efce9054
Arykov, Andrey (PPN) <andrey.arykov@mhp.com>
2025-03-12 20:52:33 +0100

    - some progress

--- 53-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 53-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = () => {
+    console.log(counter)
+    setCounter(counter + 1)
+  }
+
+
+  return (
+    <>
+      <h4>useState basics {counter}</h4>
+      <button onClick={handleClick} className="btn">Click</button>
+    </>
+  );
 };
 
 export default UseStateBasics;
--- 53-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 53-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import {people } from "../../../data"
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [persons, setPersons] = useState(people)
+
+  
+  const removeOne = () => {
+    const one = persons.pop()
+    console.log(one)
+    console.log(persons)
+    setPersons([...persons])
+  }
+
+  const clearList = () => {
+    setPersons([])
+  }
+
+  return (
+    <>
+      <h2>useState array example</h2>
+      {persons.map((p) => {
+        return <li key={p.id}>{p.name}</li>
+      })}
+      <button className="btn" onClick={removeOne}>Remove 1</button>
+      <button className="btn" onClick={clearList}>Clear list</button>
+    </>
+  );
 };
 
 export default UseStateArray;
