6eff35d77a07cd34e93cabcd397eaefd7f7903b4
aarykov <arykov1972@gmail.com>
2025-03-13 21:52:18 +0100

    - birthday reminder

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,22 @@
+import data from './data'
+import PersonList from './PersonList'
+import { useState } from 'react'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data)
+
+  console.log(people)
+
+  return (
+    <main>
+      <div className='container'>
+        <h3 className='title'>Birthday Reminder</h3>
+        <PersonList persons={people} />
+        <p className='text'>Amount of people: {people.length}</p>
+        <button type='button' className='btn btn-block' onClick={() => setPeople([])}>Clear All</button>
+      </div>
+    </main>
+  )
+  
 };
 export default App;
