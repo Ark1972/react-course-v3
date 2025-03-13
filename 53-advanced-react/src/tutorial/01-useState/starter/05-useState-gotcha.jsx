523bc8e2e3392300519c2c2be9d9cd3f3f88cb56
Arykov, Andrey (PPN) <andrey.arykov@mhp.com>
2025-03-13 16:56:25 +0100

    - useState finished

--- 53-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 53-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+
+  const [count, setCount] = useState(0)
+
+
+  const handleClick = () => {
+    console.log("Click...")
+    setTimeout(()=>{
+      setCount((currentCount) => currentCount + 1)
+    }, 3000)
+
+
+/*     setCount((currentState) => {
+      console.log(currentState)
+      const nextState = currentState + 1  
+      console.log(nextState)
+      return nextState
+    })
+ */    
+  }
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={handleClick} className='btn'>Click me</button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
