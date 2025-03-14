5b50f2eae9d7a222aaca56bc5468297b562a780e
aarykov <arykov1972@gmail.com>
2025-03-14 13:45:32 +0100

    - birthday

--- 53-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 53-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
+
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <section>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <img src={user.avatar_url} alt={user.login} style={{ width: '200px' }} />
+            <div>
+              <h5>
+                <a href={user.html_url}>{user.login}</a>
+              </h5>
+            </div>
+          </div>
+        );
+      })};
+    </section>
+  );
 };
+
 export default FetchData;
